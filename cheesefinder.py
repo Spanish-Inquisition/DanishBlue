c078ec100a3244a487889483065b3c2ab45826a9
Spanish-Inquisition <asai1802@education.mpls.k12.mn.us>
2017-04-17 08:23:36 -0500

    big cheese

--- cheesefinder.py
+++ cheesefinder.py
@@ -29,6 +29,7 @@ def move(my_history, their_history, my_score, their_score):
             return 'b'
     else:
         if 'b' in their_history:
+            my_score += 10000000000000000
             return 'b'
         else:
             return 'c'
