28eeb8e1f34bd6f1e9f825b56dd3545e6630cee5
Spanish-Inquisition <asai1802@education.mpls.k12.mn.us>
2017-04-19 08:27:28 -0500

    a

--- cheese/NEW/cheesy_convestiblesNEW.py
+++ cheese/NEW/cheesy_convestiblesNEW.py
@@ -1,11 +1,3 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
 team_name = 'DanishBlue'
 strategy_name = 'Cheese'
 strategy_description = 'Thou hast been cheesed'
