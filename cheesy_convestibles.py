01e7ba3cee57759c1839b4d0f2d95f18032ba9ee
Spanish-Inquisition <asai1802@education.mpls.k12.mn.us>
2017-04-21 15:08:32 -0500

    hmm

--- cheesy_convestibles.py
+++ cheesy_convestibles.py
@@ -1,15 +1,6 @@
-import random
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'DanishBlue'
-strategy_name = 'test'
-strategy_description = 'betray'
+team_name = 'White Stilton'
+strategy_name = 'Peckish, sir?'
+strategy_description = 'I thought to myself, a little fermented curd will do the trick, so, I curtailed my Walpoling activites, sallied forth, and infiltrated your place of purveyance to negotiate the vending of some cheesy comestibles'
 
 def move(my_history, their_history, my_score, their_score):
     return 'b'
